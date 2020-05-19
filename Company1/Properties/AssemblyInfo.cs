2103f8b27a6f1894b9241e7d888450d9e5466e59
pablo <WPawel78@gmail.com>
2020-05-19 20:01:27 +0200

    init

---
+++ Company1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Company1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Company1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("b75e62ac-a6c4-4ce6-8d9c-210ce5e54c71")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ZaawansowaneProjOb/Program.cs
@@ -0,0 +1,26 @@
+﻿using Finances.Employees;
+using System;
+using DocumentFormat.OpenXml.Bibliography;
+
+namespace Finances
+{
+    namespace Employees
+    {
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Person Person1 = new Person();
+                Empolyee Person = new Empolyee();
+
+                Person1.SetPerson("Pablo", "Wozniak", 22);
+                Person1.GetPerson();
+
+                Person.SetEmployee("cook", 4);
+                Console.WriteLine(Person.GetEmplyee());
+
+            }
+        }
+    }
+}
